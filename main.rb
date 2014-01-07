92ab086dc5c6a6d8f38dff19103351b8f80f3e86
bin.jinb <bin.jinb@alibaba-inc.com>
2014-01-07 15:02:12 +0800

    完成展示，删除基本demo

--- main.rb
+++ main.rb
@@ -1,1 +1,25 @@
-puts "welcome!"
+require 'sinatra'
+require 'slim'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Info
+  include DataMapper::Resource
+  property :id,           Serial
+  property :type,         String, :required => true
+  property :detail,       String
+  property :createtime,   DateTime
+end
+
+DataMapper.finalize
+
+get '/' do 
+  @infos = Info.all.sort_by {|i| -i.id}
+  slim :index
+end
+
+delete '/info/:id' do
+  Info.get(params[:id]).destroy
+  redirect '/'
+end
